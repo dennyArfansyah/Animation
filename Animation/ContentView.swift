877efc5aac0b1d6b053e391d715c3f2a5e280363
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-14 01:23:02 +0700

    Finalizing code for day 1

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 50, damping: 1)) {
+                animationAmount += 360.0
+            }
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0.5, y: 0.5, z: 0.5))
+        
+//        Button("Tap Me") {
+//            animationAmount += 0.5
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+//        .animation(.interpolatingSpring(stiffness: 100, damping: 1), value: animationAmount)
+        
+//        VStack {
+//            Stepper("Scale Amount", value: $animationAmount.animation(
+//                .easeInOut(duration: 1)
+//                .repeatCount(3, autoreverses: true)
+//            ), in: 1...10)
+//                .padding()
+//            Spacer()
+//
+//            Button("Tap Me") {
+//                animationAmount += 1
+//            }
+//            .padding(50)
+//            .background(.red)
+//            .foregroundColor(.white)
+//            .clipShape(Circle())
+//            .scaleEffect(animationAmount)
+//        }
     }
 }
 
