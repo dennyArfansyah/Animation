d4cc314bdd884c19695c38c76654b02d352e2215
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-14 00:42:14 +0700

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Denny Arfansyah on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
